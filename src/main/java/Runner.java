baa689f328f5ee99a4f5a835b92be8212cb559dc
Angelina Blyth <angelinalblyth@hotmail.com>
2018-07-12 16:08:38 +0100

    Added two journalists to the runner

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,2 +1,9 @@
+import models.Journalist;
+
 public class Runner {
+
+    Journalist journalist1 = new Journalist("Ron Burgundy");
+    DBHelper.save(journalist1);
+    Journalist journalist2 = new Journalist("Buck Tamland");
+    DBHelper.save(journalist2);
 }
