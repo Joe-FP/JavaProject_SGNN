a7bf9c96cc1d198ccbd9cf00892f80b22762dcdd
Angelina Blyth <angelinalblyth@hotmail.com>
2018-07-12 16:27:04 +0100

    Added DBHelper

--- src/test/java/JournalistTest.java
+++ src/test/java/JournalistTest.java
@@ -1,5 +1,10 @@
+import models.Journalist;
+import org.junit.Before;
+
 public class JournalistTest {
 
   @Before
-
+  public void setUp() throws Exception {
+    Journalist journalist1 = new Journalist("Ron Burgundy");
+  }
 }
